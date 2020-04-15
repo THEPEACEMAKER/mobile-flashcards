cefe99255eea5f9acca5effda1f2d5911d517c35
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2020-04-15 19:55:16 +0200

    add TabNavigator

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 import DeckList from './components/DeckList'
+import NewDeck from './components/NewDeck'
+import { NavigationContainer } from '@react-navigation/native'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
+const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <DeckList />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="DECKS" component={DeckList} />
+        <Tab.Screen name="NEW DECK" component={NewDeck} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
