4ecdefa5199a09a2c8e3f920e7a09f58e169ddb2
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2020-04-15 20:36:48 +0200

    add StatusBar

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StatusBar } from 'react-native'
 import DeckList from './components/DeckList'
 import NewDeck from './components/NewDeck'
 import { NavigationContainer } from '@react-navigation/native'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import Constants from 'expo-constants'
+
+function TheStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="DECKS" component={DeckList} />
-        <Tab.Screen name="NEW DECK" component={NewDeck} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={{flex: 1}}>
+      <TheStatusBar backgroundColor={'grey'} barStyle="light-content" />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="DECKS" component={DeckList} />
+          <Tab.Screen name="NEW DECK" component={NewDeck} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </View>
   )
 }
