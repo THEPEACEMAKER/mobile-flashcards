fd27a50d1860e9ecf3e138d25dde2aabc46af5c4
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2020-04-17 21:29:54 +0200

    connecting Quiz component to the store

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react"
 import { View, Text } from 'react-native'
 import QuizQuestion from './QuizQuestion'
+import { connect } from 'react-redux'
 
 class Quiz extends Component {
+	state = {
+	  displayedQuestion: 0,
+	}
+
   render() {
-    const { title } = this.props.route.params
-    const question = 'What is React?'
-    const answer = 'A library for managing user interfaces'
+  	const { questions } = this.props
+  	const { displayedQuestion } = this.state
+    const question = questions[displayedQuestion].question
+    const answer = questions[displayedQuestion].answer
     return (
       <QuizQuestion question={question} answer={answer} />
     )
   } 
 }
 
-export default Quiz
+function mapStateToProps ({ decks }, { route }) {
+  const { title } = route.params
+  const deck = decks[title]
+
+  return {
+    questions: deck.questions,
+  }
+}
+
+export default connect(mapStateToProps)(Quiz)
