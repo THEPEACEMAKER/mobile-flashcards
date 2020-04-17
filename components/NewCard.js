d3cf5f8165ec2a7b016ea66228183a78946c0be2
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2020-04-17 16:37:57 +0200

    style NewCard component

--- components/NewCard.js
+++ components/NewCard.js
@@ -1,21 +1,54 @@
 import React, { Component } from "react"
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
 
 class NewCard extends Component {
   render() {
     const { title } = this.props.route.params
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center',}}>
-        <Text>NewCard View in {title}</Text>
-        <TouchableOpacity
-          style={{margin: 10, padding: 10, borderColor: 'black', borderWidth: 1,}}
-          onPress={() => console.log('get back to Deck')}
-        >
-            <Text>Add New Card</Text>
-        </TouchableOpacity>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center',}}>
+          <TextInput
+            style={[styles.input, {margin: 0,}]}
+            placeholder="The Question"
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="The Answer"
+          />
+          <TouchableOpacity
+            style={[styles.AndroidBtn, {backgroundColor: 'black',}]}
+          >
+              <Text style={styles.submitBtnText}>Submit</Text>
+          </TouchableOpacity>
       </View>
     )
   } 
 }
+
+const styles = StyleSheet.create({
+  AndroidBtn: {
+    backgroundColor: 'white',
+    padding: 15,
+    paddingLeft: 60,
+    paddingRight: 60,
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  submitBtnText: {
+    color: 'white',
+    fontSize: 22,
+  },
+  input: {
+    width: '90%',
+    backgroundColor: 'white',
+    margin: 30,
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 8,
+    paddingLeft: 10,
+  }
+})
 
 export default NewCard
